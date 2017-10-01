11c57de01af21cc30497858ce9ea3a939bda1021
Will Ballard <wballard@mailframe.net>
2017-10-01 10:48:59 -0500

    parsing email

--- mailparser.py
+++ mailparser.py
@@ -1,19 +1,64 @@
 '''
-            # get this into a dict type format for easy access
-            message = email.message_from_string(data[0][1].decode('utf8'))
-            message_text = []
-            for part in message.walk():
-                content_type = part.get_content_type()
-                print(content_type)
-                if content_type == 'text/plain':
-                    message_text.append(part.get_payload())
-                if content_type == 'text/html':
-                    soup = BeautifulSoup(part.get_payload(), 'html.parser')
-                    message_text.append(soup.get_text())
-            final_email = dict(message.items())
-            final_email['text'] = '\n'.join(message_text)
-            print(result, final_email)
+Given RFC822 text, parse email into a more structured format.
 '''
-from bs4 import BeautifulSoup
 
 import email
+import pprint
+from pathlib import Path
+
+
+def parse(rfc822_string):
+    '''
+    Parameters
+    ----------
+    rfc822_string
+        Text of an email in RFC822 format.
+
+    Returns
+    -------
+    list
+        A list of tuples, in (name, value) format for each header,
+        along with a (text, ""), and (html, "") tuple with the body content.
+    '''
+    message = email.message_from_string(rfc822_string)
+    headers = list(map(parse_headers, message.items()))
+    message_text = []
+    message_html = []
+    for part in message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain':
+            message_text.append(part.get_payload())
+        if content_type == 'text/html':
+            message_html.append(part.get_payload())
+    text = ('text', '\n'.join(message_text))
+    html = ('html', '\n'.join(message_html))
+    return headers + [text] + [html]
+
+
+def parse_headers(header_tuple):
+    '''
+    Given a header (name, value) tuple, do additional parsing
+    for more structure on email addresses and dates.
+
+    Parameters
+    ----------
+    header_tuple
+        A (name, value) pair of strings.
+
+    Returns
+    -------
+    tuple
+        A (name, value) tuple, where value will be parsed to
+        a more structured tuple than merely a string.
+    '''
+    name, value = header_tuple
+    if name in ['To', 'From', 'Delivered-To']:
+        value = email.utils.parseaddr(value)
+    if name == 'Date':
+        value = email.utils.parsedate_tz(value)
+    return (name, value)
+
+
+if __name__ == '__main__':
+    parsed = parse(Path('./var/data/email.txt').read_text())
+    pprint.pprint(parsed)
