d7c9a4eaf03b90ee5044d4ccbb7ce3913c5e1d44
Will Ballard <wballard@mailframe.net>
2017-10-28 08:50:43 -0500

    small refactoring to get embedding in dataset

--- examples/replies_dense.py
+++ examples/replies_dense.py
@@ -1,44 +1,20 @@
-#%%
 import mailscanner
 replies = mailscanner.datasets.LabeledTextFileDataset('./replies.txt')
 
-#%%
 # one hot encode the labels
-from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
-from sklearn.pipeline import Pipeline
-label_encoder = LabelEncoder()
-onehot_encoder = Pipeline([
-    ('binarizer', LabelBinarizer()),
-    ('onehot', OneHotEncoder())
-])
-labels = label_encoder.fit_transform(replies.labels)
-# mildly tricky, need to wrap the array in an array
-targets = onehot_encoder.fit_transform(labels).toarray()
-targets.shape
+targets = replies.one_hot_labels
 
-#%%
-# take a peek at a few one hot labels
-targets[0:10]
-
-#%%
 # now set up sequencing and embedding
-from vectoria import CharacterTrigramEmbedding
-trigram = CharacterTrigramEmbedding()
-sources = trigram.sequencer.transform(replies.texts)
-sources.shape
+sources = replies.texts
 
-#%%
-# take a peek at some ngram sequences
-sources[0:10]
 
-#%%
 # data is loaded and preprocesed, now create a keras model to encode
 # Here is a very simple -- dense -- model,
 # not taking into any real consideration the structure of text,
 # or the sequential nature of words or ngrams.
 import keras
-inputs = keras.layers.Input(shape=(trigram.maxlen,))
-embedded = trigram.model(inputs)
+inputs = keras.layers.Input(shape=(replies.trigram.maxlen,))
+embedded = replies.trigram.model(inputs)
 dense_one = keras.layers.Dense(128, activation='relu')(embedded)
 dense_two = keras.layers.Dense(128, activation='relu')(dense_one)
 # softmax on two classes -- which map to our 0, 1 one hots
@@ -53,7 +29,7 @@ model.compile(
 model.summary()
 model.fit(
     x=sources,
-    y=targets, 
+    y=targets,
     validation_split=0.05,
     batch_size=32,
     epochs=16
--- mailscanner/datasets/textfiles.py
+++ mailscanner/datasets/textfiles.py
@@ -2,7 +2,11 @@
 Turn text files on disk into in memory tensors for use with machine learning.
 '''
 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder, OneHotEncoder
 from smart_open import smart_open
+from vectoria import CharacterTrigramEmbedding
+
 
 class LabeledTextFileDataset:
     '''
@@ -12,18 +16,21 @@ class LabeledTextFileDataset:
     Attributes
     ----------
     labels
-        A list of label values
+        A binay encoded array of the labels
+    one_hot_labels
+        One hot encoding of labels
     texts
-        A list of text values
+        A 2-d tensor of string and ngram positional sequence encodings.
 
     At a given index `n` `labels[n]` is the corresponding label for `texts[n]`.
 
     >>> import mailscanner
     >>> dataset = mailscanner.datasets.LabeledTextFileDataset('./var/data/labeled.txt')
     >>> dataset.labels
-    ['Good', 'Bad']
+    array([1, 0])
     >>> dataset.texts
-    ['This is a good example', 'This is a bad example']
+    array([[112284,   8220,  64853, ...,      0,      0,      0],
+           [107523,  82916, 185037, ...,      0,      0,      0]], dtype=int32)
     '''
     def __init__(self, textfile_path):
         '''
@@ -35,9 +42,20 @@ class LabeledTextFileDataset:
             A string path, which is passed to `smart_open`, so this can can a local file
             or even an S3 url.
         '''
-        self.labels = []
-        self.texts = []
+        label_buffer = []
+        text_buffer = []
         for line in smart_open(textfile_path):
             label, text = line.decode('utf8').split('\t')
-            self.labels.append(label)
-            self.texts.append(text.strip())
+            label_buffer.append(label)
+            text_buffer.append(text.strip())
+       
+        label_encoder = LabelEncoder()
+        onehot_encoder = Pipeline([
+            ('binarizer', LabelBinarizer()),
+            ('onehot', OneHotEncoder())
+        ])
+        self.labels = label_encoder.fit_transform(label_buffer)
+        # mildly tricky, need to wrap the array in an array
+        self.one_hot_labels = onehot_encoder.fit_transform(self.labels).toarray()
+        self.trigram = CharacterTrigramEmbedding()
+        self.texts = self.trigram.sequencer.transform(text_buffer)
