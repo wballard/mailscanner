9277b260b98740ffdc4d3dc5dad9a702c28c6853
Will Ballard <wballard@mailframe.net>
2017-10-30 13:21:38 -0500

    perfectly overfit

--- examples/replies_ensemble.py
+++ examples/replies_ensemble.py
@@ -7,8 +7,8 @@ targets = replies.one_hot_labels
 # now set up sequencing and embedding
 sources = replies.texts
 
-HIDDEN = 64
-ACTIVATION = 'sigmoid'
+HIDDEN = 16
+ACTIVATION = 'elu'
 
 
 # data is loaded and preprocesed, now create a keras model to encode
@@ -20,27 +20,29 @@ inputs = keras.layers.Input(shape=(replies.trigram.maxlen,))
 embedded = replies.trigram.model(inputs)
 
 # convolution to learn word and phrase like features
-conv = keras.layers.Conv1D(HIDDEN, 3, activation=ACTIVATION)(embedded)
+conv = keras.layers.Conv1D(HIDDEN, 3, activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(embedded)
 conv = keras.layers.MaxPooling1D(3)(conv)
-conv = keras.layers.Conv1D(HIDDEN, 3, activation=ACTIVATION)(conv)
+conv = keras.layers.Conv1D(HIDDEN, 3, activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(conv)
+conv = keras.layers.MaxPooling1D(3)(conv)
+conv = keras.layers.Conv1D(HIDDEN, 3, activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(conv)
 conv = keras.layers.MaxPooling1D(3)(conv)
 
 # recurrent with attention, this generates sequences
 # note this is GPU only, and keras
 # '>=2.0.9, it is shocking slow otherwise
-recurrent_forward = keras.layers.CuDNNLSTM(HIDDEN)(conv)
-recurrent_backward = keras.layers.CuDNNLSTM(HIDDEN)(mailscanner.layers.TimeStepReverse()(conv))
+recurrent_forward = keras.layers.CuDNNLSTM(HIDDEN, kernel_regularizer=keras.regularizers.l2(0.))(conv)
+recurrent_backward = keras.layers.CuDNNLSTM(HIDDEN, kernel_regularizer=keras.regularizers.l2(0.))(mailscanner.layers.TimeStepReverse()(conv))
 recurrent = keras.layers.Concatenate()([recurrent_forward, recurrent_backward])
 
 # recurrent with attention, this generates sequences
 # note this is GPU only, and keras
 # '>=2.0.9, it is shocking slow otherwise
-recurrent_sequence_forward = keras.layers.CuDNNLSTM(HIDDEN, return_sequences=True)(conv)
-recurrent_sequence_backward = keras.layers.CuDNNLSTM(HIDDEN, return_sequences=True)(mailscanner.layers.TimeStepReverse()(conv))
+recurrent_sequence_forward = keras.layers.CuDNNLSTM(HIDDEN, return_sequences=True, kernel_regularizer=keras.regularizers.l2(0.))(conv)
+recurrent_sequence_backward = keras.layers.CuDNNLSTM(HIDDEN, return_sequences=True, kernel_regularizer=keras.regularizers.l2(0.))(mailscanner.layers.TimeStepReverse()(conv))
 recurrent_sequence = keras.layers.Concatenate()([recurrent_sequence_forward, recurrent_sequence_backward])
 # now attend to the most important
-self_attention = mailscanner.layers.TimeDistributedSelfAttention(activation=ACTIVATION)(recurrent_sequence)
-time_attention = mailscanner.layers.SelfAttention(activation=ACTIVATION)(recurrent_sequence)
+self_attention = mailscanner.layers.TimeDistributedSelfAttention(activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(recurrent_sequence)
+time_attention = mailscanner.layers.SelfAttention(activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(recurrent_sequence)
 
 # now make a consistent shape and ensemble together as a stack, using global max pooling
 # to take out any remaining time steps and keep the strongest signals
@@ -50,9 +52,9 @@ time_attention = keras.layers.GlobalMaxPooling1D()(time_attention)
 ensemble = keras.layers.Concatenate()([conv, recurrent, self_attention, time_attention])
 
 # dense before final output
-stack = keras.layers.Dense(HIDDEN, activation=ACTIVATION)(ensemble)
+stack = keras.layers.Dense(HIDDEN, activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(ensemble)
 stack = keras.layers.Dropout(0.5)(stack)
-stack = keras.layers.Dense(HIDDEN, activation=ACTIVATION)(stack)
+stack = keras.layers.Dense(HIDDEN, activation=ACTIVATION, kernel_regularizer=keras.regularizers.l2(0.))(stack)
 stack = keras.layers.Dropout(0.5)(stack)
 
 # softmax on two classes -- which map to our 0, 1 one hots
@@ -68,8 +70,8 @@ model.summary()
 model.fit(
     x=sources,
     y=targets,
-    validation_split=0.05,
-    batch_size=32,
+    validation_split=0.10,
+    batch_size=128,
     epochs=256
 )
 
